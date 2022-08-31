2797667570b0c8264037925d88b6c10c85b27103
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-08-31 14:11:05 +0200

    types of data

--- src/myFirstProject.java
+++ src/myFirstProject.java
@@ -1,5 +1,10 @@
 public class myFirstProject {
     public static void main(String[] args) {
-        System.out.println("My First Project and I changed it");
+        System.out.println(10); //liczby całkowite
+        System.out.println(10.5); //zmiennoprzecinkowe z .
+        System.out.println("text"); //tekst w ""
+        System.out.println('a'); //pojedynczy znak w ''
+        System.out.println(true); //prawda
+        System.out.println(false); //fałsz
     }
 }
