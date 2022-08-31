b174ff1820602db31344d412d334e32728e6259e
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-08-31 12:50:06 +0200

    commit

---
+++ src/mainApp.java
@@ -0,0 +1,11 @@
+public class mainApp {
+
+    public static void main(String[] args) {
+
+        //punkt wyjściowy aplikacji
+
+        System.out.println("Hello World");
+
+    }
+
+}
---
+++ src/myFirstProject.java
@@ -0,0 +1,5 @@
+public class myFirstProject {
+    public static void main(String[] args) {
+        System.out.println("My First Project");
+    }
+}
